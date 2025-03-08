4173854f2373184883ec45f7d70566970bf67f7f
Abhishek-223 <abhishek223official@gmail.com>
2025-03-09 00:37:29 +0530

    Delete backend directory

--- backend/routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadReceipt } from '../controllers/uploadController';
-
-const router = express.Router();
-
-router.post('/upload', uploadReceipt);
-
-export default router;
