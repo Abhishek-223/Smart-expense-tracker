af0603c5787176ee7f281c5a60126d401e1068aa
Abhishek-223 <abhishek223official@gmail.com>
2025-03-09 00:38:01 +0530

    Delete frontend directory

--- frontend/esbuild.config.js
+++
@@ -1,8 +0,0 @@
-const esbuild = require('esbuild');
-
-esbuild.build({
-  entryPoints: ['src/index.jsx'],
-  bundle: true,
-  outfile: 'dist/bundle.js',
-  loader: { '.js': 'jsx', '.jsx': 'jsx' },
-}).catch(() => process.exit(1));
