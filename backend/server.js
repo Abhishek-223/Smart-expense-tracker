ceb74e5acc02290ed28cc7f5fb5353ed9d164eb0
Abhishek-223 <abhishek223official@gmail.com>
2025-03-12 21:17:47 +0530

    expenseContext updated

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ import connectDB from "./config/db.js"; // Import the connectDB function
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: ["http://localhost:5173", "https://smart-expense-tracker-delta.vercel.app"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Error handling middleware for JSON parsing errors
